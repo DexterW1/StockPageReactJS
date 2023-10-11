ff082734360e2ed501a8ddfd6da947eec9987da3
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-10 23:14:57 -0700

    Finished dropdown button for watchlist

--- client/src/components/DropdownButton.js
+++ client/src/components/DropdownButton.js
@@ -1,9 +1,39 @@
 import React,{useState,useRef} from 'react'
 import '../styles/DropdownButton.css'
 export default function DropdownButton() {
-    const [buttonText,setButtonText]=useState('Sort by Name');
+    const [buttonText,setButtonText]=useState("Sort by Name");
+    const [selectedItem,setSelectedItem]=useState(null);
     const [sortDirection,setSortDirection]=useState(0);
     const [isDropDownOpen,setDropDownOpen]=useState(false);
+    const [ascCheckMark,setAscCheckMark]=useState(false);
+
+    const sortList = ["Name","Symbol","Price","Day Change","Day % Change"]
+    function handleOptionClick(index,btnText){
+        if(selectedItem===index){
+            setSelectedItem(null);
+        }
+        else{
+            setSelectedItem(index);
+        }
+        setDropDownOpen(!isDropDownOpen);
+        setButtonText("Sort by "+btnText);
+
+    }
+    function handleOrderClick(index,btnText){
+        if(ascCheckMark===index){
+            setAscCheckMark(null);
+        }
+        else{
+            setAscCheckMark(index);
+        }
+        setDropDownOpen(!isDropDownOpen);
+        if(btnText==="Ascending (A-Z)"){
+            setSortDirection(0);
+        }
+        else{
+            setSortDirection(1);
+        }
+    }
     function toggleDropDown(){
         setDropDownOpen(!isDropDownOpen);
     }
@@ -22,16 +52,16 @@ export default function DropdownButton() {
                 <div className="filter-options">
                     <p>Sort By</p>
                     <ul className="dropdown-options">
-                        <li onClick={()=>{setButtonText('Sort by Name'); toggleDropDown()}}>Name</li>
-                        <li onClick={()=>{setButtonText('Sort by Symbol');toggleDropDown()}}>Symbol</li>
-                        <li onClick={()=>{setButtonText('Sort by Price');toggleDropDown()}}>Price</li>
-                        <li onClick={()=>{setButtonText('Sort by Day Change');toggleDropDown()}}>Day Change</li>
-                        <li onClick={()=>{setButtonText('Sort by Day % Change');toggleDropDown()}}>Day % Change</li>
+                        {sortList.map((item,index)=>(
+                            <li key={index} onClick={()=>{handleOptionClick(index,item)}} className={selectedItem===index?'selected':''}>{selectedItem === index && (
+                                <span className="checkmark">✔</span>
+                              )}{item}</li>
+                        ))}
                     </ul>
                     <p>Sort Direction</p>
                     <ul className='dropdown-direction'>
-                        <li onClick={()=>{setSortDirection(0)}}>Ascending (A-Z)</li>
-                        <li onClick={()=>{setSortDirection(1)}}>Descending (Z-A)</li>
+                        <li key={6} onClick={()=>{handleOrderClick(6,'Ascending (A-Z)')}}>{ascCheckMark===6 &&(<span className="checkmark">✔</span>)}Ascending (A-Z)</li>
+                        <li key={7} onClick={()=>{handleOrderClick(7,'Descending (Z-A)')}}>{ascCheckMark===7 &&(<span className="checkmark">✔</span>)}Descending (Z-A)</li>
                     </ul>
                 </div>
             )}
