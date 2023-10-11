04861ced12b9058a99705ca75fade5c41e71a571
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-10 20:56:24 -0700

    added styling for dropdown finished asc vs desc

--- client/src/components/DropdownButton.js
+++ client/src/components/DropdownButton.js
@@ -2,6 +2,7 @@ import React,{useState,useRef} from 'react'
 import '../styles/DropdownButton.css'
 export default function DropdownButton() {
     const [buttonText,setButtonText]=useState('Sort by Name');
+    const [sortDirection,setSortDirection]=useState(0);
     const [isDropDownOpen,setDropDownOpen]=useState(false);
     function toggleDropDown(){
         setDropDownOpen(!isDropDownOpen);
@@ -10,7 +11,11 @@ export default function DropdownButton() {
     <>
         <div className="dropdown-wrapper">
             <div className="btnicon-header" onClick={toggleDropDown}>
-                <ion-icon name="arrow-up-outline"></ion-icon>
+                {sortDirection === 0 ? (
+                    <ion-icon name="arrow-up-outline"></ion-icon>
+                ) : (
+                    <ion-icon name="arrow-down-outline"></ion-icon>
+                )}
                 <button className='ButtonHeader'>{buttonText}</button>
             </div>
             {isDropDownOpen && (
@@ -22,6 +27,11 @@ export default function DropdownButton() {
                         <li onClick={()=>{setButtonText('Sort by Price');toggleDropDown()}}>Price</li>
                         <li onClick={()=>{setButtonText('Sort by Day Change');toggleDropDown()}}>Day Change</li>
                         <li onClick={()=>{setButtonText('Sort by Day % Change');toggleDropDown()}}>Day % Change</li>
+                    </ul>
+                    <p>Sort Direction</p>
+                    <ul className='dropdown-direction'>
+                        <li onClick={()=>{setSortDirection(0)}}>Ascending (A-Z)</li>
+                        <li onClick={()=>{setSortDirection(1)}}>Descending (Z-A)</li>
                     </ul>
                 </div>
             )}
