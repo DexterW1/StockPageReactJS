cad47ed3fac332e8271c92df2b04a901d5e08ebf
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-21 21:42:13 -0700

    changed port number

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5003;
 const routes = require('./routes/index.js');
 require('dotenv').config();
 
