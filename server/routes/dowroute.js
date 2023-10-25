05c02af77e5259d5250d36d60884ca0e124d0f93
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-25 15:26:30 -0700

    test

--- server/routes/dowroute.js
+++ server/routes/dowroute.js
@@ -7,10 +7,11 @@ const convertData = require('../helperfunctions/converttimeunix');
 
 router.get('/dowroute',async(req,res)=>{
     const {start,end} = getTodayTimeUnix();
-    const queryOptions = {period1:start,period2:end+(5*60),interval:'5m',lang:"en-us"}
-    const result1 =await yahooFinance.chart('^IXIC',queryOptions);
-    // convertData(result1);
-    const newData = {symbol:'^IXIC',data:result1};
+    // const queryOptions = {period1:start,period2:end+(5*60),interval:'5m',lang:"en-us"}
+    const queryOptions = {period1:"2023-10-25",interval:'5m',lang:"en-us"}
+    const result1 =await yahooFinance.chart('TSLA',queryOptions);
+    convertData(result1);
+    const newData = {symbol:'TSLA',data:result1};
     res.json(newData);
 
 });
