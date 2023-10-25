9cc281a4b566b603239248d48b41344257d4095d
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-25 14:32:26 -0700

    check post errors

--- server/routes/postSymbol.js
+++ server/routes/postSymbol.js
@@ -7,13 +7,17 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey =finKey;
 const finnhubClient = new finnhub.DefaultApi();
 router.post('/sendsymbol',async (req,res)=>{
+    console.log("reqbody",req.body);
     const symbol = req.body.symbol;
+    console.log("Symbol:",symbol);
     let result  = {symbol:symbol};
     if(symbol!=''){
         result.name= FindSymbolName(symbol);
+        console.log("result.name",result.name);
     }
     finnhubClient.quote(symbol,(error,data,response)=>{
         result.data=data;
+        console.log("result:",result);
         res.json(result);
     })
 });
