659cfa6c15f85ccfb77c5205bd4da338c5bfd9c5
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-10-25 15:29:16 -0700

    testing nasdaq

--- server/routes/dowroute.js
+++ server/routes/dowroute.js
@@ -9,9 +9,9 @@ router.get('/dowroute',async(req,res)=>{
     const {start,end} = getTodayTimeUnix();
     // const queryOptions = {period1:start,period2:end+(5*60),interval:'5m',lang:"en-us"}
     const queryOptions = {period1:"2023-10-25",interval:'5m',lang:"en-us"}
-    const result1 =await yahooFinance.chart('TSLA',queryOptions);
+    const result1 =await yahooFinance.chart('^IXIC',queryOptions);
     convertData(result1);
-    const newData = {symbol:'TSLA',data:result1};
+    const newData = {symbol:'^IXIC',data:result1};
     res.json(newData);
 
 });
