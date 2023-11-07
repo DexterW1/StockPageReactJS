eb6b0ec35cec7a595a660f0efabfb0e503f0dff3
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-11-07 15:07:00 -0800

    updated url

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,129 +1,199 @@
-import React,{useEffect,useState} from 'react'
-import '../styles/MainContent.css'
-import GraphCard from './GraphCard'
-import DashboardCard from './DashboardCard';
-import NewsCard from './NewsCard';
-import axios from 'axios';
-import WatchlistContent from './WatchlistContent';
-import Loader from './loader';
-const url = "http://localhost:5003"
+import React, { useEffect, useState } from "react";
+import "../styles/MainContent.css";
+import GraphCard from "./GraphCard";
+import DashboardCard from "./DashboardCard";
+import NewsCard from "./NewsCard";
+import axios from "axios";
+import WatchlistContent from "./WatchlistContent";
+import Loader from "./loader";
+const url = "https://stock-page-server.vercel.app";
 function formatDollar(value) {
-    const roundedValue = parseFloat(value).toFixed(2);
-    return parseFloat(roundedValue).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    });
-  }
-  function formatDecimal(value){
-      return(value).toFixed(2);
-  }
-export default function MainContent({watchlistData,setWatchlistData}) {
-  const[activeButton,setactiveButton]=useState('dashboard');  
-  const[info,setInfo]=useState([]);
+  const roundedValue = parseFloat(value).toFixed(2);
+  return parseFloat(roundedValue).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+function formatDecimal(value) {
+  return value.toFixed(2);
+}
+export default function MainContent({ watchlistData, setWatchlistData }) {
+  const [activeButton, setactiveButton] = useState("dashboard");
+  const [info, setInfo] = useState([]);
   // New Dashboard
-  const[stockYahoo1,setStockYahoo1]=useState([]);
-  const[stockYahoo2,setStockYahoo2]=useState([]);
-  const[stockYahoo3,setStockYahoo3]=useState([]);
-  const[symData1,setSymData1]=useState([]);
-  const[symData2,setSymData2]=useState([]);
-  const[symData3,setSymData3]=useState([]);
-  const[rightData,setRightData]=useState([]);
-  useEffect(()=>{
+  const [stockYahoo1, setStockYahoo1] = useState([]);
+  const [stockYahoo2, setStockYahoo2] = useState([]);
+  const [stockYahoo3, setStockYahoo3] = useState([]);
+  const [symData1, setSymData1] = useState([]);
+  const [symData2, setSymData2] = useState([]);
+  const [symData3, setSymData3] = useState([]);
+  const [rightData, setRightData] = useState([]);
+  useEffect(() => {
     axios.get(`${url}/api/market-news`).then((response) => {
-        setInfo(response.data);
+      setInfo(response.data);
     });
-    axios.get(`${url}/api/trending`).then((res)=>{
-        setStockYahoo1(res.data['^DJI']);
-        setStockYahoo2(res.data['^IXIC']);
-        setStockYahoo3(res.data['^GSPC']);
-    })
-    axios.get(`${url}/api/quote`).then((res)=>{
-        setSymData1(res.data['^DJI']);
-        setSymData2(res.data['^IXIC']);
-        setSymData3(res.data['^GSPC']);
+    axios.get(`${url}/api/trending`).then((res) => {
+      setStockYahoo1(res.data["^DJI"]);
+      setStockYahoo2(res.data["^IXIC"]);
+      setStockYahoo3(res.data["^GSPC"]);
     });
-    axios.get(`${url}/api/trend`).then((res)=>{
-        setRightData(res.data);
+    axios.get(`${url}/api/quote`).then((res) => {
+      setSymData1(res.data["^DJI"]);
+      setSymData2(res.data["^IXIC"]);
+      setSymData3(res.data["^GSPC"]);
     });
-  },[]);
-//   if(!info || !stockYahoo3 || !symData3 || !rightData){
-//     return <Loader/>
-//   }
+    axios.get(`${url}/api/trend`).then((res) => {
+      setRightData(res.data);
+    });
+  }, []);
+  //   if(!info || !stockYahoo3 || !symData3 || !rightData){
+  //     return <Loader/>
+  //   }
   return (
     <>
-        <div className="main-container">
-            <div className="left-side-menu">
-                <div className="nav-container">
-                    
-                    <button id="dashboard" onClick={()=>{setactiveButton('dashboard')}}>
-                        <img className='leftIcons' src="./images/home-white.png" alt="home-icon" />
-                    </button>
-                    <button id="Graph" onClick={()=>{setactiveButton('graph')}}>
-                        <img className='leftIcons' id='graph-icon' src="./images/bar-chart-white.png" alt="graph-icon" /> 
-                    </button>
-                    <button id="watchlist" onClick={()=>{setactiveButton('watchlist')}}>
-                        <img className='leftIcons' src="./images/file-white.png" alt="watchlist-icon" />
-                    </button>
-                    
-                </div>
-            </div>
-            <div className="main-content">
-                {activeButton==='dashboard'&&(
-                    <>
-                        <div className="dashboard-card-container">
-                            <DashboardCard stockName='Dow Jones' Data={stockYahoo1} tickerSym={symData1}/>
-                            <DashboardCard stockName='NASDAQ' Data={stockYahoo2} tickerSym={symData2}/>
-                            <DashboardCard stockName='S&P 500' Data={stockYahoo3} tickerSym={symData3}/>
-                        </div>
-                        <div className="news-header">
-                            <h2>News</h2>
-                        </div>
-                        <div className='news-container'>
-                            <NewsCard info={info} />
-                        </div>
-                    </>
-                )}
-                {activeButton==='graph' && (
-                    <div className="graph-container">
-                        <div className="graph-card">
-                            <GraphCard/>
-                        </div>
-                    </div>
-                )}
-                {activeButton==='watchlist'&&(
-                    <div className="watchlist-card">
-                        <WatchlistContent setWatchlistData={setWatchlistData} watchlistData ={watchlistData} />
-                    </div>
-                )}
-            </div>
-            <div className="right-side-menu">
-                <h2 className='right-side-header'>Trending</h2>
-                {!rightData ? (<Loader/>):(
-                    <div className="right-card">
-                        {rightData.map((obj,index)=>{
-                            return(
-                                <>
-                                    <div className={index%2===1 ? 'small-cardb':'small-cardl'}>
-                                        <div className='small-header'>
-                                            <h3>{obj.symbol}</h3>
-                                            <p>{obj.data.price.shortName}</p>
-                                        </div>
-                                        <div className="small-info">
-                                            <p id="regularprice">{formatDollar(obj.data.price.regularMarketPrice)}</p>
-                                            <p className={obj.data.price.regularMarketChange < 0 ? 'colorRed':'colorGreen'} id='regularchange'>{formatDecimal(obj.data.price.regularMarketChange)}</p>
-                                            <p className={obj.data.price.regularMarketChangePercent < 0 ? 'colorRed':'colorGreen'}id="changepercentage">{formatDecimal(obj.data.price.regularMarketChangePercent*100)}%</p>
-                                        </div>
-                                    </div>
-                                </>
-                            )
-                        })}
-                    </div>
-                )}
-            </div>
+      <div className="main-container">
+        <div className="left-side-menu">
+          <div className="nav-container">
+            <button
+              id="dashboard"
+              onClick={() => {
+                setactiveButton("dashboard");
+              }}
+            >
+              <img
+                className="leftIcons"
+                src="./images/home-white.png"
+                alt="home-icon"
+              />
+            </button>
+            <button
+              id="Graph"
+              onClick={() => {
+                setactiveButton("graph");
+              }}
+            >
+              <img
+                className="leftIcons"
+                id="graph-icon"
+                src="./images/bar-chart-white.png"
+                alt="graph-icon"
+              />
+            </button>
+            <button
+              id="watchlist"
+              onClick={() => {
+                setactiveButton("watchlist");
+              }}
+            >
+              <img
+                className="leftIcons"
+                src="./images/file-white.png"
+                alt="watchlist-icon"
+              />
+            </button>
+          </div>
         </div>
+        <div className="main-content">
+          {activeButton === "dashboard" && (
+            <>
+              <div className="dashboard-card-container">
+                <DashboardCard
+                  stockName="Dow Jones"
+                  Data={stockYahoo1}
+                  tickerSym={symData1}
+                />
+                <DashboardCard
+                  stockName="NASDAQ"
+                  Data={stockYahoo2}
+                  tickerSym={symData2}
+                />
+                <DashboardCard
+                  stockName="S&P 500"
+                  Data={stockYahoo3}
+                  tickerSym={symData3}
+                />
+              </div>
+              <div className="news-header">
+                <h2>News</h2>
+              </div>
+              <div className="news-container">
+                <NewsCard info={info} />
+              </div>
+            </>
+          )}
+          {activeButton === "graph" && (
+            <div className="graph-container">
+              <div className="graph-card">
+                <GraphCard />
+              </div>
+            </div>
+          )}
+          {activeButton === "watchlist" && (
+            <div className="watchlist-card">
+              <WatchlistContent
+                setWatchlistData={setWatchlistData}
+                watchlistData={watchlistData}
+              />
+            </div>
+          )}
+        </div>
+        <div className="right-side-menu">
+          <h2 className="right-side-header">Trending</h2>
+          {!rightData ? (
+            <Loader />
+          ) : (
+            <div className="right-card">
+              {rightData.map((obj, index) => {
+                return (
+                  <>
+                    <div
+                      className={
+                        index % 2 === 1 ? "small-cardb" : "small-cardl"
+                      }
+                    >
+                      <div className="small-header">
+                        <h3>{obj.symbol}</h3>
+                        <p>{obj.data.price.shortName}</p>
+                      </div>
+                      <div className="small-info">
+                        <p id="regularprice">
+                          {formatDollar(obj.data.price.regularMarketPrice)}
+                        </p>
+                        <p
+                          className={
+                            obj.data.price.regularMarketChange < 0
+                              ? "colorRed"
+                              : "colorGreen"
+                          }
+                          id="regularchange"
+                        >
+                          {formatDecimal(obj.data.price.regularMarketChange)}
+                        </p>
+                        <p
+                          className={
+                            obj.data.price.regularMarketChangePercent < 0
+                              ? "colorRed"
+                              : "colorGreen"
+                          }
+                          id="changepercentage"
+                        >
+                          {formatDecimal(
+                            obj.data.price.regularMarketChangePercent * 100
+                          )}
+                          %
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 //<div> Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy"> Dave Gandy </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,43 +1,47 @@
-import React,{useState,useEffect,useRef, useContext} from 'react';
-import '../styles/TopBar.css';
-import axios from 'axios'
-const nasdaqTicker = './tickerData/nasdaq_tickers.json';
-const nyseTicker = '/tickerData/nyse_tickers.json';
-const nasdaqFull = './tickerData/nasdaq_full_tickers.json';
-const nyseFull = './tickerData/nyse_full_tickers.json';
-const url="http://localhost:5003";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import "../styles/TopBar.css";
+import axios from "axios";
+const nasdaqTicker = "./tickerData/nasdaq_tickers.json";
+const nyseTicker = "/tickerData/nyse_tickers.json";
+const nasdaqFull = "./tickerData/nasdaq_full_tickers.json";
+const nyseFull = "./tickerData/nyse_full_tickers.json";
+const url = "https://stock-page-server.vercel.app";
 
-export default function TopBar({watchlistData,setWatchlistData}) {
-  const [searchInput,setSearchInput]= useState('');
+export default function TopBar({ watchlistData, setWatchlistData }) {
+  const [searchInput, setSearchInput] = useState("");
   const [combinedSymb, setCombinedSymb] = useState([]);
   const [combinedFull, setCombinedFull] = useState([]);
   const [autocompleteData, setAutocompleteData] = useState([]);
-  const inputRef=useRef(null);
-  function saveDataToLocalStorage(data){
-    localStorage.setItem('watchlistdata',JSON.stringify(data));
+  const inputRef = useRef(null);
+  function saveDataToLocalStorage(data) {
+    localStorage.setItem("watchlistdata", JSON.stringify(data));
   }
-  function loadSavedData(){
-    const savedData = localStorage.getItem('watchlistdata');
-    if(savedData){
+  function loadSavedData() {
+    const savedData = localStorage.getItem("watchlistdata");
+    if (savedData) {
       setWatchlistData(JSON.parse(savedData));
     }
   }
-  function handleSearch(symbol){
-    if(symbol !=='' && !watchlistData.some(item => item.symbol === symbol)){
-      axios.post(`${url}/api/postsymbol/sendsymbol`,{symbol})
-        .then((res)=>{
+  function handleSearch(symbol) {
+    if (
+      symbol !== "" &&
+      !watchlistData.some((item) => item.symbol === symbol)
+    ) {
+      axios
+        .post(`${url}/api/postsymbol/sendsymbol`, { symbol })
+        .then((res) => {
           res.data.data.color = res.data.data.dp < 0 ? 1 : 0;
-          const prevResultsData = [...watchlistData,res.data];
+          const prevResultsData = [...watchlistData, res.data];
           setWatchlistData(prevResultsData);
           saveDataToLocalStorage(prevResultsData);
         })
-        .catch((error)=>{
+        .catch((error) => {
           console.log(error);
-        })
+        });
     }
   }
-  useEffect(()=>{
-    async function fetchData(){
+  useEffect(() => {
+    async function fetchData() {
       const resNas = await fetch(nasdaqTicker);
       const dataNas = await resNas.json();
       const resNyse = await fetch(nyseTicker);
@@ -46,8 +50,8 @@ export default function TopBar({watchlistData,setWatchlistData}) {
       setCombinedSymb(combinedData);
     }
     fetchData();
-  },[]);
-  useEffect(()=>{
+  }, []);
+  useEffect(() => {
     async function fetchData() {
       const resNas = await fetch(nasdaqFull);
       const dataNas = await resNas.json();
@@ -58,22 +62,25 @@ export default function TopBar({watchlistData,setWatchlistData}) {
     }
     fetchData();
     loadSavedData();
-  },[]);
-  useEffect(()=>{
-    if(!searchInput){
+  }, []);
+  useEffect(() => {
+    if (!searchInput) {
       setAutocompleteData([]);
       return;
     }
     const updatedData = updateResult(searchInput);
     setAutocompleteData(updatedData);
     // console.log(updatedData);
-  },[searchInput]);
+  }, [searchInput]);
 
-  function updateResult(userInput){
-    const results = combinedFull.filter((item)=>{
-      return item.symbol.includes(userInput.toUpperCase()) ||item.name.toUpperCase().includes(userInput.toUpperCase());
-    })
-    return results.slice(0,10);
+  function updateResult(userInput) {
+    const results = combinedFull.filter((item) => {
+      return (
+        item.symbol.includes(userInput.toUpperCase()) ||
+        item.name.toUpperCase().includes(userInput.toUpperCase())
+      );
+    });
+    return results.slice(0, 10);
     // return combinedFull.filter((symbol)=>symbol.symbol.includes(userInput.toUpperCase())).slice(0,10);
   }
 
@@ -83,49 +90,63 @@ export default function TopBar({watchlistData,setWatchlistData}) {
       if (
         inputRef.current &&
         !inputRef.current.contains(event.target) &&
-        !document.querySelector('.autocomplete-result').contains(event.target)
+        !document.querySelector(".autocomplete-result").contains(event.target)
       ) {
         setAutocompleteData([]); // Hide autocomplete results
       }
     }
 
     // Add the event listener when the component mounts
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     // Clean up the event listener when the component unmounts
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
   return (
     <>
-        <div className='header-container'>
-            <header>
-                <div className='logo-container'>
-                    <img src="./images/stock-exchange-app.png" alt="logo" />
-                </div>
-                <div className="search-container">
-                  <div className="control-search">
-                    <input type="search" id="search" name="search" autoComplete='off' placeholder='Search Stock In Watchlist...' ref={inputRef} onChange={(e)=>{setSearchInput(e.target.value);}} value={searchInput} />
-                    <div className="autocomplete-result">
-                    <ul className="autocomplete-list">
-                      {autocompleteData.map((obj)=>(
-                        <li key={obj.symbol} onClick={()=>{
-                          setSearchInput(obj.symbol+': '+obj.name);
-                          setAutocompleteData([]);
-                          handleSearch(obj.symbol);
-                        }}
-                      >
-                        <strong>{obj.symbol}: {obj.name}</strong>
-                      </li>
-                      ))}
-                    </ul>
-                  </div>
-                  </div>
-                </div>
-            </header>
-        </div>
-
+      <div className="header-container">
+        <header>
+          <div className="logo-container">
+            <img src="./images/stock-exchange-app.png" alt="logo" />
+          </div>
+          <div className="search-container">
+            <div className="control-search">
+              <input
+                type="search"
+                id="search"
+                name="search"
+                autoComplete="off"
+                placeholder="Search Stock In Watchlist..."
+                ref={inputRef}
+                onChange={(e) => {
+                  setSearchInput(e.target.value);
+                }}
+                value={searchInput}
+              />
+              <div className="autocomplete-result">
+                <ul className="autocomplete-list">
+                  {autocompleteData.map((obj) => (
+                    <li
+                      key={obj.symbol}
+                      onClick={() => {
+                        setSearchInput(obj.symbol + ": " + obj.name);
+                        setAutocompleteData([]);
+                        handleSearch(obj.symbol);
+                      }}
+                    >
+                      <strong>
+                        {obj.symbol}: {obj.name}
+                      </strong>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
     </>
-  )
+  );
 }
