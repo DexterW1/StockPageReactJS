d9cc69bc6fd64158662abae026473e0e7e2a8157
DexterW1 <56940477+DexterW1@users.noreply.github.com>
2024-05-17 20:52:53 -0700

    updated packages

--- server/routes/actualTrend.js
+++ server/routes/actualTrend.js
@@ -1,18 +1,23 @@
-const yahooFinance = require('yahoo-finance2').default;
-const express = require('express');
+const yahooFinance = require("yahoo-finance2").default;
+const express = require("express");
 const router = express.Router();
 
-router.get('/trend',async(req,res)=>{
-    const queryOptions= {count:30, lang:'en-US'};
-    const queryOptions1 = {modules:['price','topHoldings']};
-    let result = await yahooFinance.trendingSymbols('US',queryOptions);
-    result = result.quotes.filter((item)=> item.symbol!==':entitySlug');
-    const promise = result.map(async(item)=>{
-        const result = await yahooFinance.quoteSummary(item.symbol,queryOptions1);
-        return {data:result, symbol:item.symbol};
-    }).slice(0,17);
-    const send = await Promise.all(promise);
-    res.json(send);
+router.get("/trend", async (req, res) => {
+  const queryOptions = { count: 30, lang: "en-US" };
+  const queryOptions1 = { modules: ["price", "topHoldings"] };
+  let result = await yahooFinance.trendingSymbols("US", queryOptions);
+  result = result.quotes.filter((item) => item.symbol !== ":entitySlug");
+  const promise = result
+    .map(async (item) => {
+      const result = await yahooFinance.quoteSummary(
+        item.symbol,
+        queryOptions1
+      );
+      return { data: result, symbol: item.symbol };
+    })
+    .slice(0, 17);
+  const send = await Promise.all(promise);
+  res.json(send);
 });
 
 module.exports = router;
--- server/routes/marketNewsRoute.js
+++ server/routes/marketNewsRoute.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 const finKey = process.env.API_KEY;
 // Define an API route to fetch market news data
 
-router.get('/market-news', async (req, res) => {
+router.get("/market-news", async (req, res) => {
   try {
-    const finnhub = require('finnhub');
-    const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    const finnhub = require("finnhub");
+    const api_key = finnhub.ApiClient.instance.authentications["api_key"];
     api_key.apiKey = finKey;
     console.log(finKey);
-    const finnhubClient = new finnhub.DefaultApi()
+    const finnhubClient = new finnhub.DefaultApi();
     finnhubClient.marketNews("general", {}, (error, data, response) => {
-      res.json(data.slice(0,10));
+      if (data) {
+        res.json(data.slice(0, 10));
+      }
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  } 
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
