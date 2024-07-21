760ffe6e37c00543adb22623219f0b3c5654a282
DexterW1 <56940477+DexterW1@users.noreply.github.com>
2024-07-21 16:20:32 -0700

    Revert "fix symbols not rendering at times"
    This reverts commit 800ed3543af22c862b720212e017daaf983dbcc5.

--- client/src/components/ChartCard.js
+++ client/src/components/ChartCard.js
@@ -1,98 +1,70 @@
-import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ReferenceLine,
-} from "recharts";
-import { format } from "date-fns-tz";
+import React,{useState,useEffect, useRef} from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine } from 'recharts';
+import { format } from 'date-fns-tz';
 function formatYAxisLabel(value) {
-  const roundedValue = parseFloat(value).toFixed(2);
-  return parseFloat(roundedValue).toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+    const roundedValue = parseFloat(value).toFixed(2);
+    return parseFloat(roundedValue).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
 }
-function formatDecimal(value) {
+function formatDecimal(value){
   // if(value===null) return 0;
-  return value.toFixed(2);
+    return(value).toFixed(2);
 }
 function formatTimestamp(timestamp) {
-  return format(new Date(timestamp * 1000), "HH:mm", {
-    timeZone: "America/Los_Angeles",
-  });
+    return format(new Date(timestamp*1000),'HH:mm',{timeZone: 'America/Los_Angeles'});
 }
 function CustomTooltip({ active, payload }) {
-  if (active && payload && payload.length) {
-    const dataPoint = payload[0].payload; // Get the data point
-    return (
-      <div className="custom-tooltip" style={{ margin: 10 }}>
-        <p style={{ fontSize: 12 }}>{`Time: ${dataPoint.date}`}</p>
-        <p style={{ fontSize: 12 }}>{`Close Price: ${formatYAxisLabel(
-          dataPoint.close
-        )}`}</p>
-      </div>
-    );
-  }
-  return null;
-}
-
-export default function ChartCard(props) {
-  const { SymbolData } = props;
-  // console.log(SymbolData);
-  if (!SymbolData || !SymbolData.quotes) {
-    // Handle the case where data is not available
+    if (active && payload && payload.length) {
+      const dataPoint = payload[0].payload; // Get the data point
+      return (
+        <div className="custom-tooltip" style={{margin:10}}>
+          <p style={{fontSize: 12}}>{`Time: ${dataPoint.date}`}</p>
+          <p style={{fontSize:12}}>{`Close Price: ${formatYAxisLabel(dataPoint.close)}`}</p>
+        </div>
+      );
+    }
     return null;
   }
-  const convertedData = SymbolData.quotes
-    .filter((item) => item.close !== null) // Filter out items with null values
-    .map((item) => ({
+
+export default function ChartCard(props) {
+    const {SymbolData} = props;
+    // console.log(SymbolData);
+    if (!SymbolData || !SymbolData.quotes) {
+      // Handle the case where data is not available
+      return null; 
+    }
+    const convertedData = SymbolData.quotes
+    .filter(item => item.close !== null) // Filter out items with null values
+    .map(item => ({
       ...item,
       close: formatDecimal(item.close),
       date: formatTimestamp(item.date),
     }));
-  const chartMargin = { top: 20, right: 0, bottom: 6, left: 20 }; // Set all margins to 0
-  const lastDataPoint = convertedData[convertedData.length - 1].close;
-  const referenceLineY = convertedData[0].close;
-  const referenceLineStroke = lastDataPoint > referenceLineY ? "green" : "red";
+    const chartMargin = { top: 20, right:0, bottom: 6, left: 20 }; // Set all margins to 0
+    const lastDataPoint = convertedData[convertedData.length - 1].close;
+    const referenceLineY = convertedData[0].close;
+    const referenceLineStroke = lastDataPoint > referenceLineY ? 'green' : 'red';
   return (
     <>
-      <div className="linechart-container">
-        <LineChart
-          width={280}
-          height={150}
-          data={convertedData}
-          margin={chartMargin}
-        >
-          <XAxis dataKey="date" />
-          <YAxis
-            type="number"
-            domain={["dataMin", "dataMax"]}
-            tickFormatter={formatYAxisLabel}
-            tick={{ fontSize: 12 }}
-          />
-          <Tooltip
-            content={CustomTooltip}
-            wrapperStyle={{ background: "none" }}
-          />
-          <Line
-            type="monotone"
-            dataKey="close"
-            name="Close Price"
-            stroke={referenceLineStroke}
-            dot={false}
-          />
-          <ReferenceLine
-            y={convertedData[0].close}
-            stroke="grey"
-            strokeDasharray="5 5"
-          />
-        </LineChart>
-      </div>
+        <div className="linechart-container">
+                <LineChart width={280} height={150} data={convertedData} margin={chartMargin}>
+                    <XAxis dataKey="date" />
+                    <YAxis 
+                        type="number"
+                        domain={['dataMin','dataMax']}
+                        tickFormatter={formatYAxisLabel}
+                        tick={{fontSize:12}}
+                    />
+                    <Tooltip content={CustomTooltip} wrapperStyle={{background:'none'}}/>
+                    <Line type="monotone" dataKey="close" name="Close Price" stroke={referenceLineStroke} dot={false}/>
+                    <ReferenceLine y={convertedData[0].close} stroke="grey" strokeDasharray="5 5" />
+                </LineChart>
+        </div>
+    
     </>
-  );
+  )
 }
--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/NewsCard.css";
 import Loader from "./loader";
 export default function NewsCard(props) {
